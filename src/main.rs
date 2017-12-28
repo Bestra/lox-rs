65bc134fb47bbc387c3a5e469a57adc3c3d5f8cb
Chris Westra <cwestra@gmail.com>
2017-12-27 23:10:33 -0500

    Repl actually loops

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ use lox::parser::Parser;
 use lox::ast::AstPrint;
 use lox::interpreter::Interpreter;
 fn main() {
-    let input = rprompt::prompt_reply_stdout(">").unwrap();
-
-    let mut scanner = lox::Scanner::new(input);
-    scanner.scan_tokens();
-
-    let mut parser = Parser::new(scanner.tokens);
-    let output = parser.parse();
-    println!("{}", output.pretty_print());
-
     let mut interpreter = Interpreter::new();
-    match interpreter.interpret(output) {
-        Ok(o) => println!("{}", o),
-        Err(o) => eprintln!("{}", o),
+    loop {
+        let input = rprompt::prompt_reply_stdout(">").unwrap();
+
+        let mut scanner = lox::Scanner::new(input);
+        scanner.scan_tokens();
+
+        let mut parser = Parser::new(scanner.tokens);
+        let output = parser.parse();
+        println!("{}", output.pretty_print());
+
+        match interpreter.interpret(output) {
+            Ok(o) => println!("{}", o),
+            Err(o) => eprintln!("{}", o),
+        }
     }
 }
